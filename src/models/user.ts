b4be86d7e10a6c0fff987c4c4ba8d24f8145b4df
Evgeny Tupikov <bwinter@yandex.ru>
2021-07-29 22:34:08 +0500

    Fix user model

--- src/models/user.ts
+++ src/models/user.ts
@@ -4,17 +4,29 @@ export class User {
   username: string | null;
   firstName: string | null;
   lastName: string | null;
+  birthDate: string | null;
+  email: string | null;
+  avatar: string | null;
+  isActive: boolean | null;
 
   constructor({
     id = null,
     username = null,
     firstName = null,
     lastName = null,
+    birthDate = null,
+    email = null,
+    avatar = null,
+    isActive = null,
   }) {
     this.id = id;
     this.username = username;
     this.firstName = firstName;
     this.lastName = lastName;
+    this.birthDate = birthDate;
+    this.email = email;
+    this.avatar = avatar;
+    this.isActive = isActive;
   }
 
 }
