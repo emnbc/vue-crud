bced5e725342fa18145948d7b08f9ec879b847f6
Evgeny Tupikov <bwinter@yandex.ru>
2021-09-17 21:40:23 +0500

    Main Menu component

---
+++ src/components/MainMenu.vue
@@ -0,0 +1,42 @@
+<template>
+    <v-navigation-drawer v-model="drawer" app clipped>
+        <v-list dense>
+            <v-list-item :to="item.link" v-for="(item, i) in itemList" :key="i" link>
+            <v-list-item-action>
+                <v-icon>{{item.icon}}</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+                <v-list-item-title>{{item.text}}</v-list-item-title>
+            </v-list-item-content>
+            </v-list-item>
+            <v-divider inset class="mt-2"></v-divider>
+            <v-subheader inset>Information</v-subheader>
+            <v-list-item to="/about" link>
+            <v-list-item-action>
+                <v-icon>mdi-cog</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+                <v-list-item-title>About</v-list-item-title>
+            </v-list-item-content>
+            </v-list-item>
+        </v-list>
+    </v-navigation-drawer>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class MainMenu extends Vue {
+    drawer: boolean | null = null;
+
+    itemList = [
+        { icon: 'mdi-home', text: 'Home', link: '/home'},
+        { icon: 'mdi-view-dashboard', text: 'Users', link: '/users'},
+    ];
+
+    toggleMenu(): void {
+        this.drawer = !this.drawer;
+    }
+}
+</script>
--- src/views/Main.vue
+++ src/views/Main.vue
@@ -1,31 +1,10 @@
 <template>
   <v-app id="inspire">
 
-      <v-navigation-drawer v-model="drawer" app clipped>
-        <v-list dense>
-          <v-list-item :to="item.link" v-for="(item, i) in itemList" :key="i" link>
-            <v-list-item-action>
-              <v-icon>{{item.icon}}</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>{{item.text}}</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-          <v-divider inset class="mt-2"></v-divider>
-          <v-subheader inset>Information</v-subheader>
-          <v-list-item to="/about" link>
-            <v-list-item-action>
-              <v-icon>mdi-cog</v-icon>
-            </v-list-item-action>
-            <v-list-item-content>
-              <v-list-item-title>About</v-list-item-title>
-            </v-list-item-content>
-          </v-list-item>
-        </v-list>
-      </v-navigation-drawer>
+      <main-menu ref="menu" />
 
       <v-app-bar app clipped-left color="blue-grey" dark>
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+        <v-app-bar-nav-icon @click.stop="toggleMenu()"></v-app-bar-nav-icon>
         <v-toolbar-title>Application</v-toolbar-title>
         <v-spacer></v-spacer>
         <v-btn class="ma-2" text icon>
@@ -53,24 +32,20 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 import { logOut } from '../utils/auth';
-import router from '@/router';
+import MainMenu from '@/components/MainMenu.vue'
 
-@Component
+@Component({
+  components: {'main-menu': MainMenu}
+})
 export default class Main extends Vue {
-  drawer = null;
   logOut = logOut;
-
-  itemList = [
-    { icon: 'mdi-home', text: 'Home', link: '/home'},
-    { icon: 'mdi-view-dashboard', text: 'Users', link: '/users'},
-  ];
 
   created(): void {
     this.$vuetify.theme.dark = false;
   }
 
-  toSignIn(): void {
-    router.replace('/sign-in');
+  toggleMenu(): void {
+    (this.$refs.menu as MainMenu).toggleMenu();
   }
 
 }
